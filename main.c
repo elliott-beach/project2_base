22ce87711e259635cf350538be85fa8b0009a60e
Elliott Beach <elliott2.71828@gmail.com>
2017-11-10 11:39:06 -0600

    indentation was bothering me

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 /*
-Main program for the virtual memory project.
-Make all of your modifications to this file.
-You may add or rearrange any code or data as you need.
-The header files page_table.h and disk.h explain
-how to use the page table and disk interfaces.
-*/
+   Main program for the virtual memory project.
+   Make all of your modifications to this file.
+   You may add or rearrange any code or data as you need.
+   The header files page_table.h and disk.h explain
+   how to use the page table and disk interfaces.
+   */
 
 #include "page_table.h"
 #include "disk.h"
@@ -36,16 +36,15 @@ void fifo_handler(struct page_table *pt, int page ) {
 
     // Case: page does not have write permissions
     if(pt->page_bits[page] == PROT_READ) {
-      page_table_set_entry(pt, page, pt->page_mapping[page], PROT_READ|PROT_WRITE);
-      num_faults++;
-      return;
+        page_table_set_entry(pt, page, pt->page_mapping[page], PROT_READ|PROT_WRITE);
+        num_faults++;
+        return;
     }
 
     if(curr_frame < nframes) {
         frame = curr_frame;
         evict = false;
-    }
-    else {
+    } else {
         // If we evict a page from a frame, this is the frame
         frame = curr_frame % pt->nframes;
         for (int curr_page=0; curr_page < pt->npages; curr_page++){
@@ -65,29 +64,29 @@ void fifo_handler(struct page_table *pt, int page ) {
 }
 
 void random_handler(struct page_table *pt, int page ) {
-	int frame = -1;
+    int frame = -1;
 
-	// Case: page does not have write permissions
-	if(pt->page_bits[page] == PROT_READ) {
-	    page_table_set_entry(pt, page, pt->page_mapping[page], PROT_READ|PROT_WRITE);
-	    num_faults++;
-	    return;
-	}
-	
-	for(int f=0;f<pt->nframes;f++){
+    // Case: page does not have write permissions
+    if(pt->page_bits[page] == PROT_READ) {
+        page_table_set_entry(pt, page, pt->page_mapping[page], PROT_READ|PROT_WRITE);
+        num_faults++;
+        return;
+    }
 
-		// Find a frame that is not already allocated to a page.
-		bool success = true;
-		for (int curr_page=0; curr_page < pt->npages; curr_page++){
-			if(pt->page_mapping[curr_page] == f && pt->page_bits[curr_page] != 0){
+    for(int f=0;f<pt->nframes;f++){
+
+        // Find a frame that is not already allocated to a page.
+        bool success = true;
+        for (int curr_page=0; curr_page < pt->npages; curr_page++){
+            if(pt->page_mapping[curr_page] == f && pt->page_bits[curr_page] != 0){
                 success = false;
-				break;
-			}
-		} if(success){
-			frame = f;
-			break;
-		}
-	}
+                break;
+            }
+        } if(success){
+            frame = f;
+            break;
+        }
+    }
     if(frame == -1){
         // Pick a random page that owns a frame.
         int curr_page;
@@ -113,65 +112,65 @@ void loadFrameIntoPage(struct page_table *pt, int page, int frame) {
 void evictPage(struct page_table *pt, int page) {
     // Only write to disk if the page has been modified
     if(pt->page_bits[page] == (PROT_READ|PROT_WRITE)) {
-      int frame = pt->page_mapping[page];
-      num_writes++;
-      disk_write(disk, page, page_table_get_physmem(pt) + frame * BLOCK_SIZE);
+        int frame = pt->page_mapping[page];
+        num_writes++;
+        disk_write(disk, page, page_table_get_physmem(pt) + frame * BLOCK_SIZE);
     }
     page_table_set_entry(pt, page, 0, 0);
 }
 
 int main( int argc, char *argv[] ) {
-	if(argc!=5) {
-		printf("use: virtmem <npages> <nframes> <rand|fifo|custom> <sort|scan|focus>\n");
-		return 1;
-	}
+    if(argc!=5) {
+        printf("use: virtmem <npages> <nframes> <rand|fifo|custom> <sort|scan|focus>\n");
+        return 1;
+    }
 
-	int npages = atoi(argv[1]);
-	nframes = atoi(argv[2]);
-        const char* algorithm = argv[3];
-	const char *program = argv[4];
+    int npages = atoi(argv[1]);
+    nframes = atoi(argv[2]);
+    const char* algorithm = argv[3];
+    const char *program = argv[4];
 
-	disk = disk_open("myvirtualdisk",npages);
-	if(!disk) {
-		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
-		return 1;
-	}
+    disk = disk_open("myvirtualdisk",npages);
+    if(!disk) {
+        fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
+        return 1;
+    }
 
 
-        page_fault_handler_t handler;
-        if(!strcmp(algorithm, "rand")){
-            handler = random_handler;
-        } else if(!strcmp(algorithm, "fifo")){
-            handler = fifo_handler;
-        }else {
-            printf("Unknown paging algorithm!\n");
-            exit(1);
-        }
+    page_fault_handler_t handler;
+    if(!strcmp(algorithm, "rand")){
+        handler = random_handler;
+    } else if(!strcmp(algorithm, "fifo")){
+        handler = fifo_handler;
+    }else {
+        printf("Unknown paging algorithm!\n");
+        exit(1);
+    }
 
-	struct page_table *pt = page_table_create( npages, nframes, handler );
-	if(!pt) {
-		fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
-		return 1;
-	}
+    struct page_table *pt = page_table_create( npages, nframes, handler );
+    if(!pt) {
+        fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
+        return 1;
+    }
 
-	char *virtmem = page_table_get_virtmem(pt);
+    char *virtmem = page_table_get_virtmem(pt);
 
-	if(!strcmp(program,"sort")) {
-		sort_program(virtmem, npages * PAGE_SIZE);
+    if(!strcmp(program,"sort")) {
+        sort_program(virtmem, npages * PAGE_SIZE);
 
-	} else if(!strcmp(program,"scan")) {
-		scan_program(virtmem,npages*PAGE_SIZE);
+    } else if(!strcmp(program,"scan")) {
+        scan_program(virtmem,npages*PAGE_SIZE);
 
-	} else if(!strcmp(program,"focus")) {
-		focus_program(virtmem,npages*PAGE_SIZE);
+    } else if(!strcmp(program,"focus")) {
+        focus_program(virtmem,npages*PAGE_SIZE);
 
-	} else {
-		fprintf(stderr,"unknown program: %s\n",argv[3]);
-	}
+    } else {
+        fprintf(stderr,"unknown program: %s\n",argv[3]);
+    }
 
-        printf("%-5d%-5d%-5d\n", num_reads, num_writes, num_faults);
-	page_table_delete(pt);
-	disk_close(disk);
+    printf("%-5d%-5d%-5d\n", num_reads, num_writes, num_faults);
+    page_table_delete(pt);
+    disk_close(disk);
 
-	return 0;
+    return 0;
 }
